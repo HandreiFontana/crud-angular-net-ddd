fc1a221492767767451bfe2977e6cd3b998ad86d
HandreiFontana <HandreiFontana>
2023-05-17 20:16:58 -0300

    feat: added IStateService and implemented

---
+++ backend/Domain/Interfaces/ServicesInterfaces/IStateService.cs
@@ -0,0 +1,18 @@
+﻿using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.ServicesInterfaces
+{
+    public interface IStateService
+    {
+        Task CreateState(State state);
+
+        Task UpdateState(State state);
+
+        Task<List<State>> ListState();
+    }
+}
---
+++ backend/Domain/Services/ServiceState.cs
@@ -0,0 +1,55 @@
+﻿using Domain.Interfaces;
+using Domain.Interfaces.ServicesInterfaces;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    internal class ServiceState : IStateService
+    {
+
+        private readonly IState _IState;
+
+        public ServiceState(IState IState)
+        {
+            _IState = IState;
+        }
+
+        public async Task CreateState(State state)
+        {
+            var validateStateName = state.ValidatePropertyString(state.StateName, "StateName");
+            var validateFederativeUnit = state.ValidatePropertyString(state.FederativeUnit, "FederativeUnit");
+
+            if (validateStateName && validateFederativeUnit)
+            {
+                state.CreatedAt = DateTime.Now;
+                state.UpdatedAt = DateTime.Now;
+
+                await _IState.Create(state);
+            }
+        }
+
+        public async Task<List<State>> ListState()
+        {
+            return await _IState.List();
+        }
+
+        public async Task UpdateState(State state)
+        {
+            var validateStateName = state.ValidatePropertyString(state.StateName, "StateName");
+            var validateFederativeUnit = state.ValidatePropertyString(state.FederativeUnit, "FederativeUnit");
+
+            if (validateStateName && validateFederativeUnit)
+            {
+                state.CreatedAt = DateTime.Now;
+                state.UpdatedAt = DateTime.Now;
+
+                await _IState.Update(state);
+            }
+        }
+    }
+}
