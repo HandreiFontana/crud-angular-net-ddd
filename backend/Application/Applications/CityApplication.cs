0a50eec7fbd3c20d99b40cba53eb5f2460274834
HandreiFontana <HandreiFontana>
2023-05-17 21:21:05 -0300

    feat [backend]: added city and state application

---
+++ backend/Application/Applications/CityApplication.cs
@@ -0,0 +1,64 @@
+﻿using Domain.Interfaces.ServicesInterfaces;
+using Domain.Interfaces;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Interfaces;
+
+namespace Application.Applications
+{
+    public class CityApplication : ICityApplication
+    {
+        ICity _ICity;
+        ICityService _ICityService;
+
+        public CityApplication(ICity ICity, ICityService ICityService)
+        {
+            _ICity = ICity;
+            _ICityService = ICityService;
+        }
+
+        public async Task CreateCity(City city)
+        {
+            await _ICityService.CreateCity(city);
+        }
+
+        public async Task<List<City>> ListCity()
+        {
+            return await _ICityService.ListCity();
+        }
+
+        public async Task UpdateCity(City city)
+        {
+            await _ICityService.UpdateCity(city);
+        }
+
+        public async Task Create(City Object)
+        {
+            await _ICity.Create(Object);
+        }
+
+        public async Task Delete(City Object)
+        {
+            await _ICity.Delete(Object);
+        }
+
+        public async Task<City> Get(int Id)
+        {
+            return await _ICity.Get(Id);
+        }
+
+        public async Task<List<City>> List()
+        {
+            return await _ICity.List();
+        }
+
+        public async Task Update(City Object)
+        {
+            await _ICity.Update(Object);
+        }
+    }
+}
---
+++ backend/Application/Applications/StateApplication.cs
@@ -0,0 +1,65 @@
+﻿using Application.Interfaces;
+using Domain.Interfaces;
+using Domain.Interfaces.ServicesInterfaces;
+using Entidades.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Applications
+{
+    public class StateApplication : IStateApplication
+    {
+
+        IState _IState;
+        IStateService _IStateService;
+
+        public StateApplication(IState IState, IStateService IStateService) 
+        {
+            _IState = IState;
+            _IStateService = IStateService;
+        }
+
+        public async Task CreateState(State state)
+        {
+            await _IStateService.CreateState(state);
+        }
+
+        public async Task<List<State>> ListState()
+        {
+            return await _IStateService.ListState();
+        }
+
+        public async Task UpdateState(State state)
+        {
+            await _IStateService.UpdateState(state);
+        }
+
+        public async Task Create(State Object)
+        {
+            await _IState.Create(Object);
+        }
+
+        public async Task Delete(State Object)
+        {
+            await _IState.Delete(Object);
+        }
+
+        public async Task<State> Get(int Id)
+        {
+            return await _IState.Get(Id);
+        }
+
+        public async Task<List<State>> List()
+        {
+            return await _IState.List();
+        }
+
+        public async Task Update(State Object)
+        {
+            await _IState.Update(Object);
+        }
+    }
+}
